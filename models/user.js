a874adf49262ddd13d4d2ca346229d03e3e29b6c
Golden Kalu <goldenlemmy@gmail.com>
2021-07-22 21:10:48 -0700

    login validation works with david and jenelle

--- models/user.js
+++ models/user.js
@@ -14,7 +14,7 @@ const userSchema = mongoose.Schema({
     minlength: 5,
     maxlength: 50,
   },
-  password: { type: String, required: true, minlength: 5, maxlength: 25 },
+  password: { type: String, required: true, minlength: 5, maxlength: 1000 },
   friends: {type: Array, default: []},
   //   friends: [{ type: Schema.Types.ObjectId, ref: "friends" }],
 });
@@ -23,9 +23,11 @@ const User = mongoose.model("user", userSchema);
 
 function validateUser(user) {
   const Schema = Joi.object({
-    name: Joi.string().min(5).max(100).required(),
+    firstName: Joi.string().min(5).max(100).required(),
+    lastName: Joi.string().min(5).max(100).required(),
+    username: Joi.string().min(5).max(100).required(),
     email: Joi.string().min(5).max(50).required().email(),
-    password: Joi.string().min(5).max(25).required(),
+    password: Joi.string().min(5).max(1000).required(),
   });
   return Schema.validate(user);
 }
