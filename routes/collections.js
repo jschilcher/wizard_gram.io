17188bebda372a8cb37b2711380471eca8ffc62b
DCarr40 <davidwcarruthers@yahoo.com>
2021-07-26 19:49:22 -0400

    added unfollow friend endpoint

--- routes/collections.js
+++ routes/collections.js
@@ -190,4 +190,23 @@ router.put("/:id/follow", async (req, res) => {
   } else res.status(403).json("You Can't friend yourself");
 });
 
+//unfollow user
+router.put("/:id/unfollow", async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      const currentUser = await User.findById(req.body.userId);
+      if (user.friends.includes(req.body.userId)) {
+        await user.updateOne({ $pull: { friends: req.body.userId } });
+        await currentUser.updateOne({ $pull: { friends: req.body.userId } });
+        res.status(200).json("You are now not following this user!");
+      } else {
+        res.status(403).json("you don't follow this user");
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else res.status(403).json("You can't unfollow yourself");
+});
+
 module.exports = router;
